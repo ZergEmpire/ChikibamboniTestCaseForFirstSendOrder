e459434beaf5418ca8b191d46093cd2f922f9139
ZergEmpire <71401820+ZergEmpire@users.noreply.github.com>
2021-09-23 16:50:11 +0300

    First Commit
    Автотест готов и настроен на 100%

---
+++ src/test/java/FirstTestCase/TestClass.java
@@ -0,0 +1,21 @@
+package FirstTestCase;
+
+import org.junit.Test;
+
+public class TestClass extends TestBase {
+
+    @Test
+    public void userCanSendOrder() {
+        openURL();
+        mathRandomHead();
+        pickRandCards();
+        addCardToBasket();
+        goBasket();
+        selectDeliveryTypePickUp();
+        selectTerminalForPickUp();
+        fillInFields();
+        selectPayType();
+        sendOrder();
+        waitForComplete();
+    }
+}
